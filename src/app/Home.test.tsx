c695674d96a97ac257466899d507ea7013fbb0d1
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-09-17 11:22:49 +0200

    check active section by manually setting it

--- src/app/Home.test.tsx
+++ src/app/Home.test.tsx
@@ -4,78 +4,71 @@ import Footer from "@/components/Footer"
 import Header from "@/components/Header"
 import Navigation from "@/components/Navigation"
 import ProjectSection from "@/components/ProjectSection"
-import { AboutMeType, ConnectType, ProjectType } from "@/utils/types"
 import { render, screen, fireEvent, waitFor } from "@testing-library/react"
 import { useInView } from "framer-motion"
 import { AboutMockData, ConnectMockData, mockProjects } from "@/utils/mockData"
 
 
-jest.mock("framer-motion", () => ({
-  useInView: jest.fn(),
-}));
-
-const ProjectsMockData: ProjectType[] = mockProjects;
+// jest.mock("framer-motion", () => ({
+//   useInView: jest.fn(),
+// }));
 
 describe("Navigation functionality tests", () => {
   const mockOnScrollToSection = jest.fn();
-  beforeEach(() => {
-    (useInView as jest.Mock).mockReset();
-  });
-  
-  it("highlights the correct navigation item based on the section in view", async () => {
-      // Mock the useInView hook to simulate different sections being in view
-      (useInView as jest.Mock).mockImplementation((ref) => {
-        // Simulate different sections being in view based on the ref
-        if (ref.current?.classList.contains("aboutMe")) return { inView: true };
-        if (ref.current?.classList.contains("project")) return { inView: false };
-        if (ref.current?.classList.contains("connect")) return { inView: false };
-        return { inView: false };
-      });
 
+//   beforeEach(() => {
+//     (useInView as jest.Mock).mockReset();
+//   });
+  
+  it("highlights About Me navigation item when About Me section is in view", async () => {
+    render(
+      <body className="flex flex-col w-full min-h-screen bg-black">
+        <Header />
+        <Navigation onScrollToSection={mockOnScrollToSection} activeSection="aboutMe" />
+        <AboutMe {...AboutMockData}/>
+        <ProjectSection projects={mockProjects}/>
+        <Connect  {...ConnectMockData}/>
+        <Footer />
+      </body>
+    )
+
+    expect(screen.getByTestId("desktop-aboutMe").classList.contains("border-b-2")).toBe(true); // aboutMe starts as highlighted
+    expect(screen.getByTestId("desktop-projects").classList.contains("border-b-2")).toBe(false);
+    expect(screen.getByTestId("desktop-connect").classList.contains("border-b-2")).toBe(false);
+
+  });
+
+  it("highlights Projects navigation item when Project section is in view", async () => {
     render(
       <body className="flex flex-col w-full min-h-screen bg-black">
       <Header />
-      <Navigation onScrollToSection={mockOnScrollToSection} activeSection="aboutMe" />
+      <Navigation onScrollToSection={mockOnScrollToSection} activeSection="projects" />
       <AboutMe {...AboutMockData}/>
-      <ProjectSection projects={ProjectsMockData}/>
+      <ProjectSection projects={mockProjects}/>
       <Connect  {...ConnectMockData}/>
       <Footer />
       </body>
     )
 
-    expect(screen.getByTestId("mobile-aboutMe")).toHaveClass("underline");
-    expect(screen.getByTestId("mobile-projects")).not.toHaveClass("underline");
-    expect(screen.getByTestId("mobile-connect")).not.toHaveClass("underline");
-
-    // Simulate scrolling to "Projects" section
-    // (useInView as jest.Mock).mockImplementation((ref) => {
-    //   if (ref === aboutMeRef.current) return { inView: false };
-    //   if (ref === projectRef.current) return { inView: true };
-    //   if (ref === connectRef.current) return { inView: false };
-    //   return { inView: false };
-    // });
-
-    // // Wait for state update
-    // await waitFor(() => {
-    //   expect(screen.getByTestId("mobile-aboutMe")).not.toHaveClass("underline");
-    //   expect(screen.getByTestId("mobile-projects")).toHaveClass("underline");
-    //   expect(screen.getByTestId("mobile-connect")).not.toHaveClass("underline");
-    // });
-
-    // // Simulate scrolling to "Connect" section
-    // (useInView as jest.Mock).mockImplementation((ref) => {
-    //   if (ref === aboutMeRef.current) return { inView: false };
-    //   if (ref === projectRef.current) return { inView: false };
-    //   if (ref === connectRef.current) return { inView: true };
-    //   return { inView: false };
-    // });
-
-    // // Wait for state update
-    // await waitFor(() => {
-    //   expect(screen.getByTestId("mobile-aboutMe")).not.toHaveClass("underline");
-    //   expect(screen.getByTestId("mobile-projects")).not.toHaveClass("underline");
-    //   expect(screen.getByTestId("mobile-connect")).toHaveClass("underline");
-
-    // });
+    expect(screen.getByTestId("desktop-aboutMe").classList.contains("border-b-2")).toBe(false);
+    expect(screen.getByTestId("desktop-projects").classList.contains("border-b-2")).toBe(true);
+    expect(screen.getByTestId("desktop-connect").classList.contains("border-b-2")).toBe(false);
   });
-})
+
+  it("highlights Connect navigation item when Connect section is in view", async () => {
+    render(
+      <body className="flex flex-col w-full min-h-screen bg-black">
+      <Header />
+      <Navigation onScrollToSection={mockOnScrollToSection} activeSection="connect" />
+      <AboutMe {...AboutMockData}/>
+      <ProjectSection projects={mockProjects}/>
+      <Connect  {...ConnectMockData}/>
+      <Footer />
+      </body>
+    )
+
+    expect(screen.getByTestId("desktop-aboutMe").classList.contains("border-b-2")).toBe(false);
+    expect(screen.getByTestId("desktop-projects").classList.contains("border-b-2")).toBe(false);
+    expect(screen.getByTestId("desktop-connect").classList.contains("border-b-2")).toBe(true);
+  });
+});
